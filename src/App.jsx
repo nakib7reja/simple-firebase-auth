27a28bd296a5d614a21f337b5d7a3bc78f590f08
NokibReja <nakib7reja@gmail.com>
2025-12-06 17:30:35 +0600

    install react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-red-500'>Simple firebase auth react</h1>
+      
+    </>
+  )
+}
+
+export default App
