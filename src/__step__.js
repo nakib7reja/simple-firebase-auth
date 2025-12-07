f89f155a4d873566539d914f16bc0a518a348b32
NokibReja <nakib7reja@gmail.com>
2025-12-07 23:27:40 +0600

    firebase setup

---
+++ src/__step__.js
@@ -0,0 +1,8 @@
+/*
+1. create a Firebase project
+2. build authentication > google sign in -- enable
+3. project settings : web application
+4. firebase install
+5. config (by copy as a beginner )
+6. in the config: init the auth of firebase application
+*/ 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth } from '../firebase/firebase.init'
+
+const googleProvider = new GoogleAuthProvider()
 
 const Login = () => {
+    const [user, setUser] = useState(null)
+    const handleGoogleSignIn = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                console.log(result.user)
+                setUser(result.user)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    const handleSingOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log('sing out done')
+                setUser(null)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
     return (
         <div>
             <h2>Please Login</h2>
+            {/* <button onClick={handleGoogleSignIn} className='btn'>Sign in with Google</button>
+            <button onClick={handleSingOut} className='btn'>Sign Out</button> */}
+            {user ?
+                <button onClick={handleSingOut} className='btn'>Sign Out</button> :
+                <button onClick={handleGoogleSignIn} className='btn'>Sign in with Google</button>
+            }
+            {user &&
+                <div>
+                    <h3>{user.displayName}</h3>
+                </div>
+            }
         </div>
     );
 };
